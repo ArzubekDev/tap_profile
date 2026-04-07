511f9abf4491c161db30763c1b114fe117919156
ArzubekDev <arzubekmain@gmail.com>
2026-04-07 13:59:00 +0600

    feat: IconHearFavorite

---
+++ components/Icons/components/IconHeartFavorite.tsx
@@ -0,0 +1,45 @@
+import cn from 'classnames';
+import { FC } from 'react';
+
+import {
+  ICON_DEFAULT_CLASSNAME,
+  ICON_DEFAULT_COLOR,
+  ICON_DEFAULT_HEIGHT,
+  ICON_DEFAULT_WIDTH,
+} from '../consts';
+
+import { TIcon } from '../types';
+
+const IconFavorite: FC<TIcon> = ({ color, className, width, height, ...rest }) => {
+  return (
+    <svg
+      {...rest}
+      width={width || ICON_DEFAULT_WIDTH}
+      height={height || ICON_DEFAULT_HEIGHT}
+      fill={color || ICON_DEFAULT_COLOR}
+      className={cn(ICON_DEFAULT_CLASSNAME, className)}
+      viewBox="0 0 28 28"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+        stroke="#000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M12 5.5V21"
+        stroke="#000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        stroke-dasharray="2 4"
+      ></path>
+    </svg>
+  );
+};
+
+export default IconFavorite;
