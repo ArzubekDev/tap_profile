663bf81243d29e8d65005b9f200e1dd4fd316a04
ArzubekDev <arzubekmain@gmail.com>
2026-04-07 11:08:17 +0600

    fix: history

--- components/pages/profile/history/index.tsx
+++ components/pages/profile/history/index.tsx
@@ -1,7 +1,22 @@
-import { Input, Select } from 'antd';
+"use client"
+import type { CalendarProps } from 'antd';
+import { Calendar, Input, Select, theme  } from 'antd';
+import type { Dayjs } from 'dayjs';
 import style from './style.module.scss';
 
+const onPanelChange = (value: Dayjs, mode: CalendarProps<Dayjs>['mode']) => {
+  console.log(value.format('YYYY-MM-DD'), mode);
+};
+
 const History = () => {
+  const { token } = theme.useToken();
+
+  const wrapperStyle: React.CSSProperties = {
+    width: 300,
+    border: `1px solid ${token.colorBorderSecondary}`,
+    borderRadius: token.borderRadiusLG,
+  };
+
   return (
     <div className={style.history}>
       <h3>История заказов</h3>
@@ -10,14 +25,14 @@ const History = () => {
           <div className={style.innerTop}>
             <label htmlFor="name" className={style.label}>
               Поиск
-              </label>
-              <Input
-                id="name"
-                className={style.searchInput}
-                placeholder="Имя, фамилия, телефон..."
-                maxLength={150}
-                variant="borderless"
-              />
+            </label>
+            <Input
+              id="name"
+              className={style.searchInput}
+              placeholder="Имя, фамилия, телефон..."
+              maxLength={150}
+              required
+            />
           </div>
           <div className={style.innerTop}>
             <label htmlFor="status" className={style.label}>
@@ -48,10 +63,42 @@ const History = () => {
             />
           </div>
         </div>
-        {/* <div className={style.center}>
-          <label htmlFor=""></label>
+        <div className={style.center}>
+          <label htmlFor="status" className={style.label}>
+            Магазины
+          </label>
+          <Select
+            className={style.searchInput}
+            defaultValue="all"
+            options={[
+              { value: 'all', label: 'Все магазины' },
+
+              { value: 'new', label: 'Новый' },
+              { value: 'confirmed', label: 'Подтвержден' },
+              { value: 'pending_payment', label: 'Ожидает оплаты' },
+              { value: 'paid', label: 'Оплачен' },
+
+              { value: 'processing', label: 'В обработке' },
+
+              { value: 'shipped', label: 'Передан в доставку' },
+              { value: 'delivering', label: 'Доставляется' },
+              { value: 'delivered', label: 'Доставлен' },
+
+              { value: 'canceled', label: 'Отменен' },
+              { value: 'refund', label: 'Возврат' },
+
+              { value: 'completed', label: 'Завершён' },
+            ]}
+          />
         </div>
-        <div className={style.bottom}></div> */}
+        <div className={style.bottom}>
+          <div className={style.innerBottom}>
+            <label htmlFor="from">Дата с</label>
+            <div style={wrapperStyle}>
+              <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   );
