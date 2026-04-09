f957b96de337a5c6f20c72d93563b0847393e177
ArzubekDev <arzubekmain@gmail.com>
2026-04-09 15:51:46 +0600

    style: selectFormController

--- components/pages/profile/history/index.tsx
+++ components/pages/profile/history/index.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { SelectFormController } from '@/components/form/formControllers';
 import InputController from '@/components/form/formControllers/inputFormController';
 import { zodResolver } from '@hookform/resolvers/zod';
 import type { DatePickerProps } from 'antd';
@@ -7,7 +8,6 @@ import { useForm } from 'react-hook-form';
 import Orders from '../orders';
 import style from './style.module.scss';
 import { ZhistoryForm } from './zod/zod';
-import { SelectFormController } from '@/components/form/formControllers';
 
 const onChange: DatePickerProps['onChange'] = (date, dateString) => {
   console.log(date, dateString);
@@ -17,15 +17,24 @@ const statusOptions = [
   { value: 'all', label: 'Все статусы' },
   { value: 'new', label: 'Новый' },
   { value: 'confirmed', label: 'Подтвержден' },
+  { value: 'pending_payment', label: 'Ожидает оплаты' },
+  { value: 'paid', label: 'Оплачен' },
+  { value: 'processing', label: 'В обработке' },
+  { value: 'shipped', label: 'Передан в доставку' },
+  { value: 'delivering', label: 'Доставляется' },
+  { value: 'delivered', label: 'Доставлен' },
+  { value: 'canceled', label: 'Отменен' },
+  { value: 'refund', label: 'Возврат' },
+  { value: 'completed', label: 'Завершён' },
 ];
 
 const History: React.FC = () => {
-const { control, handleSubmit, reset } = useForm({
+  const { control, handleSubmit, reset } = useForm({
     resolver: zodResolver(ZhistoryForm),
     defaultValues: { name: '', status: 'all', store: 'all' },
   });
 
-  const onSubmit = (data: any) => console.log("history", data);
+  const onSubmit = (data: any) => console.log('history', data);
   return (
     <section className={style.history}>
       <h3 className={style.title}>История заказов</h3>
@@ -41,14 +50,13 @@ const { control, handleSubmit, reset } = useForm({
             />
           </div>
           <div className={style.innerTop}>
-           <SelectFormController
-            name="status"
-            control={control}
-            label="Статус"
-            options={statusOptions}
-            className={style.searchInput}
-          />
-            
+            <SelectFormController
+              name="status"
+              control={control}
+              label="Статус"
+              options={statusOptions}
+              className={style.searchInput}
+            />
           </div>
         </div>
         <div className={style.center}>
@@ -63,19 +71,6 @@ const { control, handleSubmit, reset } = useForm({
 
               { value: 'new', label: 'Новый' },
               { value: 'confirmed', label: 'Подтвержден' },
-              { value: 'pending_payment', label: 'Ожидает оплаты' },
-              { value: 'paid', label: 'Оплачен' },
-
-              { value: 'processing', label: 'В обработке' },
-
-              { value: 'shipped', label: 'Передан в доставку' },
-              { value: 'delivering', label: 'Доставляется' },
-              { value: 'delivered', label: 'Доставлен' },
-
-              { value: 'canceled', label: 'Отменен' },
-              { value: 'refund', label: 'Возврат' },
-
-              { value: 'completed', label: 'Завершён' },
             ]}
           />
         </div>
