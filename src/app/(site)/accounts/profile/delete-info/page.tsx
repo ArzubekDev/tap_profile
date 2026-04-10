ba4e239caf4fdfda12bbc634d458c9f9673b69b4
ArzubekDev <arzubekmain@gmail.com>
2026-04-10 12:02:47 +0600

    feat: DeleteAccount

---
+++ src/app/(site)/accounts/profile/delete-info/page.tsx
@@ -0,0 +1,11 @@
+import DeleteAccount from '@/src/components/pages/profile/deleteAccount'
+
+const page = () => {
+  return (
+    <div>
+        <DeleteAccount/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/pages/profile/deleteAccount/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeleteAccount = () => {
+  return (
+    <section>
+DeleteAccount
+    </section>
+  )
+}
+
+export default DeleteAccount
