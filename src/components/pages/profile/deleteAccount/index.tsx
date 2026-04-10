778c1764b3771693c7abe8e55d7725db090ea8c2
ArzubekDev <arzubekmain@gmail.com>
2026-04-10 12:53:17 +0600

    fix: deleteAccount

--- src/components/pages/profile/deleteAccount/index.tsx
+++ src/components/pages/profile/deleteAccount/index.tsx
@@ -1,6 +1,6 @@
+import IconCheck from '@/src/components/Icons/components/IconCheck';
 import { Button } from 'antd';
 import style from './style.module.scss';
-import IconCheck from '@/src/components/Icons/components/IconCheck';
 
 const DeleteAccount = () => {
   return (
@@ -14,18 +14,34 @@ const DeleteAccount = () => {
         </p>
       </div>
       <div className={style.content}>
-        <h3 className={style.contentTitle}>При удалении аккаунта будут безвозвратно стерты:</h3>
-        <h5 className={style.contentInfo}><span><IconCheck/></span>История ваших покупок</h5>
-        <h5 className={style.contentInfo}>Номер телефона</h5>
-        <h5 className={style.contentInfo}>Уведомления и ФИО</h5>
-        <h5 className={style.contentInfo}>Пол и адрес проживания</h5>
+        <h3 className={style.contentTitle}>
+          При удалении аккаунта будут безвозвратно стерты:
+        </h3>
+        <h5 className={style.contentInfo}>
+          <IconCheck className={style.iconCheck} />
+          История ваших покупок
+        </h5>
+        <h5 className={style.contentInfo}>
+          <IconCheck className={style.iconCheck} />
+          Номер телефона
+        </h5>
+        <h5 className={style.contentInfo}>
+          <IconCheck className={style.iconCheck} />
+          Уведомления и ФИО
+        </h5>
+        <h5 className={style.contentInfo}>
+          <IconCheck className={style.iconCheck} />
+          Пол и адрес проживания
+        </h5>
       </div>
       <div className={style.footer}>
         <div className={style.footerTextContainer}>
           <h3 className={style.footerTitle}>Служба поддержки</h3>
-          <p className={style.footerSubTitle}>Если у вас остались вопросы или возникли трудности или хотите удалить аккаунт:</p>
+          <p className={style.footerSubTitle}>
+            Если у вас остались вопросы или возникли трудности или хотите удалить аккаунт:
+          </p>
         </div>
-        <Button type='primary'>Написать нам</Button>
+        <Button type="primary">Написать нам</Button>
       </div>
     </section>
   );
