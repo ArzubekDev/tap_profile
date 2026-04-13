62c0c2d7f36c7aae7db784e3e607d1c9381aaa03
ArzubekDev <arzubekmain@gmail.com>
2026-04-13 12:47:57 +0600

    refactor: use FORM_REQUIRED_TEXT contstants for createStore/zod/zod.ts

--- src/components/pages/createStore/zod/zod.ts
+++ src/components/pages/createStore/zod/zod.ts
@@ -1,11 +1,12 @@
+import { FORM_REQUIRED_TEXT } from "@/src/components/form/consts";
 import z from "zod";
 
 export const ZcreateStore = z.object({
-    storeName: z.string().min(1, "*Напишите название магазина*").max(150),
-    phone: z.string().min(1, "*Напишите номер телефона*").max(150),
-    whatsapp: z.string().min(1, "*Напишите Ватсап номер*"),
-    instagram: z.string().min(1, "*Напишите свой инстаграм*"),
-    address: z.string().min(1, "*Укажите адрес*"),
+    storeName: z.string().min(1, FORM_REQUIRED_TEXT).max(150),
+    phone: z.string().min(1, FORM_REQUIRED_TEXT).max(150),
+    whatsapp: z.string().min(1, FORM_REQUIRED_TEXT),
+    instagram: z.string().min(1, FORM_REQUIRED_TEXT),
+    address: z.string().min(1, FORM_REQUIRED_TEXT),
     isEveryday: z.boolean(),
     workingHours: z.tuple([z.any(), z.any()]),
     logo: z.any().optional(),
