2e4adaef01310accde833fba18e69785587058b0
ArzubekDev <arzubekmain@gmail.com>
2026-04-14 15:57:16 +0600

    style: KorzinaDropDown

--- src/shared/ui/KorzinaDropDown/KorzinaDropDown.tsx
+++ src/shared/ui/KorzinaDropDown/KorzinaDropDown.tsx
@@ -1,11 +1,13 @@
 import { Button } from 'antd';
+import { useRouter } from 'next/navigation';
 import style from './style.module.scss';
 
 const KorzinaDropDown = () => {
+    const route = useRouter()
   return (
-    <div className={style.content}>
+    <div className={style.kozinaDropDown}>
       <h4>Общая сумма: 32 KGS</h4>
-      <Button type="primary">Перейти в корзину</Button>
+      <Button onClick={() => route.push("/basket")} type="primary">Перейти в корзину</Button>
     </div>
   );
 };
--- src/shared/ui/KorzinaUI/KorzinaUI.tsx
+++ src/shared/ui/KorzinaUI/KorzinaUI.tsx
@@ -1,12 +1,9 @@
-'use client';
 import { IconCart } from '@/src/components/Icons';
 import PopoverLayout from '@/src/components/layout/popover/PopoverLayout';
-import { useRouter } from 'next/navigation';
 import style from './style.module.scss';
 import KorzinaDropDown from '../KorzinaDropDown/KorzinaDropDown';
 
 const KorzinaUI = () => {
-  const route = useRouter();
   return (
     <PopoverLayout
       content={KorzinaDropDown}
@@ -14,7 +11,7 @@ const KorzinaUI = () => {
       trigger="hover"
       innerClassName={style.profilePopoverContent}
     >
-      <div onClick={() => route.push('/basket')} className={style.navItem}>
+      <div className={style.navItem}>
         <IconCart className={style.navItemIcon} />
         <span className={style.navItemText}>Корзина</span>
       </div>
