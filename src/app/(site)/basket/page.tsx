3111b5e8b3cfa7e428ae41d244aa8c8608584f70
ArzubekDev <arzubekmain@gmail.com>
2026-04-14 15:37:09 +0600

    feat(basket): integrate with SSR

--- src/app/(site)/basket/page.tsx
+++ src/app/(site)/basket/page.tsx
@@ -1,9 +1,20 @@
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
 import Basket from '@/src/components/pages/basket'
-import React from 'react'
+import { getProducts } from '@/src/shared/api/product.api'
+import { PRODUCT_KEYS } from '@/src/shared/api/query-keys'
 
-const BasketPage = () => {
+const BasketPage = async () => {
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery({
+    queryKey: PRODUCT_KEYS.all,
+    queryFn: getProducts
+  })
+
   return (
-    <Basket/>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Basket/>
+    </HydrationBoundary>
   )
 }
 
