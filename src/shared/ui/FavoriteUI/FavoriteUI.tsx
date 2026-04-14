9fb5f9373bac9242d9ca0e70fae7621b4245f603
ArzubekDev <arzubekmain@gmail.com>
2026-04-14 15:02:50 +0600

    feat: create BasketPage

--- src/shared/ui/FavoriteUI/FavoriteUI.tsx
+++ src/shared/ui/FavoriteUI/FavoriteUI.tsx
@@ -1,9 +1,13 @@
+'use client'
+
 import { IconFavorite } from '@/src/components/Icons';
 import style from './style.module.scss';
+import { useRouter } from 'next/navigation';
 
 const FavoriteUI = () => {
+  const route = useRouter()
   return (
-    <div className={style.navItem}>
+    <div onClick={() => route.push("/basket/favorite-list")} className={style.navItem}>
       <IconFavorite className={style.navItemIcon} />
       <span className={style.navItemText}>Избранное</span>
     </div>
--- src/shared/ui/KorzinaUI/KorzinaUI.tsx
+++ src/shared/ui/KorzinaUI/KorzinaUI.tsx
@@ -1,9 +1,12 @@
+'use client'
 import { IconCart } from '@/src/components/Icons';
 import style from './style.module.scss';
+import { useRouter } from 'next/navigation';
 
 const KorzinaUI = () => {
+  const route = useRouter()
   return (
-    <div className={style.navItem}>
+    <div onClick={() => route.push('/basket')} className={style.navItem}>
       <IconCart className={style.navItemIcon} />
       <span className={style.navItemText}>Корзина</span>
     </div>
