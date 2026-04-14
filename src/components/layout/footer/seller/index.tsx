7cbdaf9bc1af0c5ec2f1af42176af35f103bda21
ArzubekDev <arzubekmain@gmail.com>
2026-04-14 16:26:49 +0600

    feat(footer): create new page Seller

---
+++ src/components/layout/footer/seller/index.tsx
@@ -0,0 +1,16 @@
+import style from '../style.module.scss'
+
+const Seller = () => {
+  return (
+     <div className={style.block}>
+                <h5>Продавцам</h5>
+                <h6>О нас</h6>
+                <h6>Как открыть магазин</h6>
+                <h6>Как продавать на маркетплейсе</h6>
+                <h6>Безопасность</h6>
+                <h6>Политика конфиденциальности</h6>
+            </div>
+  )
+}
+
+export default Seller
