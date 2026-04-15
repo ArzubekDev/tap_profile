5b9088a0fd2eab409a03d2bed61086e63512d0cf
ArzubekDev <arzubekmain@gmail.com>
2026-04-15 15:50:10 +0600

    refactor: cleaned imports

--- src/app/(site)/basket/favorite-list/layout.tsx
+++ src/app/(site)/basket/favorite-list/layout.tsx
@@ -1,23 +1,23 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
-import ProfileNav from "@/src/components/pages/profile/ProfileNav";
+import ProfileNav from '@/src/components/pages/profile/ProfileNav';
 
 import style from './style.module.scss';
 
 interface FavListLayoutProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
-const FavListLayout = ({children}: FavListLayoutProps) => {
-    return (
+const FavListLayout = ({ children }: FavListLayoutProps) => {
+  return (
     <div className="container">
       <div className={style.accountLayout}>
         <main className={style.main}>{children}</main>
-<div className={style.sidebar}>
-        <ProfileNav />
-</div>
+        <div className={style.sidebar}>
+          <ProfileNav />
+        </div>
       </div>
     </div>
-    )
-}
-export default FavListLayout
+  );
+};
+export default FavListLayout;
