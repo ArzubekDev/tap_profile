9e6c8463c207468702d915ba02f49476b2c105d0
ArzubekDev <arzubekmain@gmail.com>
2026-04-16 17:04:26 +0600

    feat; creata PaymentsPage

---
+++ src/app/(site)/info/payments/page.tsx
@@ -0,0 +1,10 @@
+import Payments from '@/src/components/pages/info/payments'
+import React from 'react'
+
+const PaymentsPage = () => {
+  return (
+    <div><Payments/></div>
+  )
+}
+
+export default PaymentsPage
