8645780eea73cac5e0cc1a3e8b523878ceb5bdd8
ArzubekDev <arzubekmain@gmail.com>
2026-04-16 17:29:32 +0600

    feat: create a ReturnsPage

---
+++ src/app/(site)/info/returns/page.tsx
@@ -0,0 +1,10 @@
+import Returns from '@/src/components/pages/info/returns'
+import React from 'react'
+
+const ReturnsPage = () => {
+  return (
+    <div><Returns/></div>
+  )
+}
+
+export default ReturnsPage
