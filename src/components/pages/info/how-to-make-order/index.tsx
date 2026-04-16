1de8b7893208a8fdee6b11a619367fe41e267da5
ArzubekDev <arzubekmain@gmail.com>
2026-04-16 16:16:36 +0600

    ref: info

--- src/components/pages/info/how-to-make-order/index.tsx
+++ src/components/pages/info/how-to-make-order/index.tsx
@@ -1,10 +1,9 @@
-import style from "./style.module.scss"
+import style from './style.module.scss';
 
 const MakeOrder = () => {
-  
   return (
-        <div className={style.makeOrder}>
-      <h2 className={style.mainTitle}>Возврат товара</h2>
+    <div className={style.makeOrder}>
+      <h2 className={style.mainTitle}>Как сделать заказ</h2>
 
       {conditions.map((item, idx) => (
         <div key={idx} className={style.infoBlocks}>
@@ -15,7 +14,9 @@ const MakeOrder = () => {
           {item.points && (
             <ul className={style.list}>
               {item.points.map((point, index) => (
-                <li key={index} className={style.item}>{point}</li>
+                <li key={index} className={style.item}>
+                  {point}
+                </li>
               ))}
             </ul>
           )}
@@ -31,7 +32,7 @@ export default MakeOrder;
 
 const conditions = [
   {
-    title: 'Условия возврата',
+    title: 'Как сделать заказ',
     subtitle: 'Вы можете вернут товар в течение 14 дней после получения',
     points: ['товар alkdfjlkd', 'второй пункт', 'третий пункт'],
   },
@@ -50,6 +51,6 @@ const conditions = [
     subtitle: 'Вы можете вернут товар в течение 14 дней после получения',
     points: ['товар dskfljk', 'второй пункт', 'третий пункт'],
     description:
-      ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, non! Impedit porro seddeserunt magnam voluptatibus ad molestias assumenda rerum omnis dolore tenetur minima exipsam labore officiis saepe, repellendus non adipisci dolor. Eligendi nobis, tenetur magnam cumque distinctio voluptates aliquam dolores saepe sit optio repudiandae eius voluptatibus tempora accusamus nam totam dolor laborum, consequuntur ipsa culpa consectetur libero numquam in. Et voluptate maxime itaque in porro quasi tempore id nemo dolores minima labore odit eius impedit, architecto totam pariatur, amet eveniet vitae nulla autem ex sunt dolorum dolor corporis? Illo at iusto corporis eligendi quod in. Rem, culpa libero.',
+      ' Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, non! Impedit porro seddeserunt magnam voluptatibus ad molestias assumenda rerum omnis dolore tenetur minima exipsam labore officiis saepe, repellendus non adipisci dolor. Eligendi nobis, tenetur magnam cumque distinctio voluptates aliquam dolores saepe sit optio repudiandae eius voluptatibus tempora accusamus nam totam dolor laborum, consequuntur ipsa culpa consectetur libero numquam in. Et voluptate maxime itaque in porro quasi tempore id nemo dolores minima labore odit eius impedit, architecto totam pariatur, amet eveniet vitae nulla autem ex sunt dolorum dolor corporis? Illo at iusto corporis eligendi quod in. Rem, culpa libero.  Настоящая политика описывает, какие данные могут собираться и обрабатываться при использовании маркетплейса Tap.kg (далее — «Сервис»), в каких целях они используются и какие у пользователя есть возможности. Документ является рабочим шаблоном и подлежит согласованию с юристом перед публикацией в юрисдикции вашей деятельности.',
   },
 ];
