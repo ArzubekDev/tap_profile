c8c384d5e7d490436396642da465917ad000b2d8
ArzubekDev <arzubekmain@gmail.com>
2026-04-16 13:07:10 +0600

    feat: added react-sticky-box

--- src/components/pages/info/InfoLayout/InfoLayout.tsx
+++ src/components/pages/info/InfoLayout/InfoLayout.tsx
@@ -1,19 +1,23 @@
+'use client'
 import { ReactNode } from "react"
+import StickyBox from "react-sticky-box"
 import InfoSidebar from "../infoSidebar"
-
 import style from "./style.module.scss"
-
 
 interface InfoLayoutProps {
     children: ReactNode
 }
-const InfoLayout = ({children}:  InfoLayoutProps) => {
+
+const InfoLayout = ({ children }: InfoLayoutProps) => {
     return (
         <div className={style.infoLayout}>
-        <main className={style.main}>{children}</main>
-        <div className={style.infoSidebar}>
-            <InfoSidebar/>
-        </div>
+            <main className={style.main}>{children}</main>
+            
+            <StickyBox offsetTop={120} offsetBottom={20} className={style.infoSidebarWrapper}>
+                <div className={style.infoSidebar}>
+                    <InfoSidebar />
+                </div>
+            </StickyBox>
         </div>
     )
 }
