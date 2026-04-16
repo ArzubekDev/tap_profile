8fe87c515ea2736fe43f8398f916c933023cfe61
ArzubekDev <arzubekmain@gmail.com>
2026-04-16 14:59:06 +0600

    feat: privacy-policy

---
+++ src/app/(site)/info/privacy-policy/page.tsx
@@ -0,0 +1,10 @@
+import PrivacyPolicy from '@/src/components/pages/info/privacy-policy'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div><PrivacyPolicy/></div>
+  )
+}
+
+export default page
--- src/components/pages/info/InfoLayout/InfoLayout.tsx
+++ src/components/pages/info/InfoLayout/InfoLayout.tsx
@@ -1,27 +1,26 @@
-'use client'
-import { ReactNode } from "react"
-import StickyBox from "react-sticky-box"
-import InfoSidebar from "@/src/shared/ui/InfoSidebar/InfoSidebar"
-import style from "./style.module.scss"
+'use client';
+import InfoSidebar from '@/src/shared/ui/InfoSidebar/InfoSidebar';
+import { ReactNode } from 'react';
+import StickyBox from 'react-sticky-box';
+import style from './style.module.scss';
 
 interface InfoLayoutProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
 const InfoLayout = ({ children }: InfoLayoutProps) => {
-    return (
-<div className="container">
-            <div className={style.infoLayout}>
-            <main className={style.main}>{children}</main>
-            
-            <StickyBox offsetTop={120} offsetBottom={20} className={style.infoSidebarWrapper}>
-                <div className={style.infoSidebar}>
-                    <InfoSidebar />
-                </div>
-            </StickyBox>
-        </div>
-</div>
-    )
-}
+  return (
+    <div className="container">
+      <div className={style.infoLayout}>
+        <main className={style.main}>{children}</main>
+        <StickyBox offsetTop={120} offsetBottom={20} className={style.infoSidebarWrapper}>
+          <div className={style.infoSidebar}>
+            <InfoSidebar />
+          </div>
+        </StickyBox>
+      </div>
+    </div>
+  );
+};
 
-export default InfoLayout
+export default InfoLayout;
