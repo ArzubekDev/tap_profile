4e5823eacb6c9e3306d8af0affc2fdc8915deb30
ArzubekDev <arzubekmain@gmail.com>
2026-04-16 16:04:21 +0600

    fix: added StickyBox in basket/history

--- src/app/(site)/basket/history/layout.tsx
+++ src/app/(site)/basket/history/layout.tsx
@@ -1,21 +1,24 @@
-import ProfileNav from "@/src/components/pages/profile/ProfileNav";
-import { ReactNode } from "react";
+import ProfileNav from '@/src/components/pages/profile/ProfileNav';
+import { ReactNode } from 'react';
 import style from './style.module.scss';
+import StickyBox from 'react-sticky-box';
 
 interface HistoryLayoutProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
-const HistoryLayout = ({children}: HistoryLayoutProps) => {
-    return (
+const HistoryLayout = ({ children }: HistoryLayoutProps) => {
+  return (
     <div className="container">
       <div className={style.accountLayout}>
         <main className={style.main}>{children}</main>
-        <div className={style.sidebar}>
-          <ProfileNav />
-        </div>
+        <StickyBox offsetTop={120} offsetBottom={20} className={style.infoSidebarWrapper}>
+          <div className={style.sidebar}>
+            <ProfileNav />
+          </div>
+        </StickyBox>
       </div>
     </div>
-    )
-}
-export default HistoryLayout
+  );
+};
+export default HistoryLayout;
