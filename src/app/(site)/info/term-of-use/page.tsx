3be2f099875b3e452f9bdcced1beae619472bae5
ArzubekDev <arzubekmain@gmail.com>
2026-04-16 18:10:36 +0600

    feat: create a new path

---
+++ src/app/(site)/info/term-of-use/page.tsx
@@ -0,0 +1,10 @@
+import UserAgreement from '@/src/components/pages/info/term-of-use'
+import React from 'react'
+
+const UserAgreementPage = () => {
+  return (
+    <div><UserAgreement/></div>
+  )
+}
+
+export default UserAgreementPage
