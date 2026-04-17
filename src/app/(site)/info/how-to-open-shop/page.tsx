41cd2414fea9c827f1c85683613df7de4b648352
ArzubekDev <arzubekmain@gmail.com>
2026-04-17 15:39:10 +0600

    feat: create htos

---
+++ src/app/(site)/info/how-to-open-shop/page.tsx
@@ -0,0 +1,10 @@
+import HowToOpenShop from '@/src/components/pages/info/how-to-open-shop'
+import React from 'react'
+
+const HowToOpenShopPage = () => {
+  return (
+    <div><HowToOpenShop/></div>
+  )
+}
+
+export default HowToOpenShopPage
