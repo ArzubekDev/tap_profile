0eb2de86cab14a6573ca4193de9c5b88ef2bc265
ArzubekDev <arzubekmain@gmail.com>
2026-04-17 15:59:32 +0600

    feat: howtosellpage

---
+++ src/app/(site)/info/how-to-sell/page.tsx
@@ -0,0 +1,10 @@
+import HowToSell from '@/src/components/pages/info/how-to-sell'
+import React from 'react'
+
+const HowToSellPage = () => {
+  return (
+    <div><HowToSell/></div>
+  )
+}
+
+export default HowToSellPage
