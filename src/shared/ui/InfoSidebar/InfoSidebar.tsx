4d448031f37439868062113b0b0c2e359e5ce61e
ArzubekDev <arzubekmain@gmail.com>
2026-04-17 15:57:18 +0600

    feat: infoSidebar

--- src/shared/ui/InfoSidebar/InfoSidebar.tsx
+++ src/shared/ui/InfoSidebar/InfoSidebar.tsx
@@ -2,7 +2,7 @@ import cn from 'classnames';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-import { PATH_ABOUT, PATH_DELIVERY, PATH_HOW_TO_MAKE_ORDER, PATH_HOW_TO_OPEN_SHOP, PATH_PAYMENTS, PATH_PRIVACY_POLICY, PATH_RETURNS, PATH_TERM_OF_USE } from '@/src/shared/consts/paths';
+import { PATH_ABOUT, PATH_DELIVERY, PATH_HOW_TO_MAKE_ORDER, PATH_HOW_TO_OPEN_SHOP, PATH_HOW_TO_SELL, PATH_PAYMENTS, PATH_PRIVACY_POLICY, PATH_RETURNS, PATH_TERM_OF_USE } from '@/src/shared/consts/paths';
 import style from './style.module.scss';
 
 const InfoSidebar = () => {
@@ -81,7 +81,7 @@ const seller: TInfoSidebar[] = [
   {
     id: 8,
     title: 'Как продавать на маркетплейсе',
-    link: '#',
+    link: PATH_HOW_TO_SELL,
   },
   {
     id: 9,
