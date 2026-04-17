eaa180a242ee8bfb0f1e4dff7bbbc3b0e266bd41
ArzubekDev <arzubekmain@gmail.com>
2026-04-17 15:45:00 +0600

    refactor: htos

--- src/components/pages/info/how-to-open-shop/index.tsx
+++ src/components/pages/info/how-to-open-shop/index.tsx
@@ -5,13 +5,13 @@ import Image from 'next/image';
 const HowToOpenShop = () => {
   return (
     <section className={style.openshop}>
-      <h2>Как открыть магазин в маркетплейсе Tap.kg</h2>
+      <h2 className={style.headline}>Как открыть магазин в маркетплейсе Tap.kg</h2>
       <div className={style.block}>
         <Fragment>
-          <h3>ШАГ 1</h3>
-          <p>Вы нажимаете свой профиль и выбираете пункт "Создать магазин"</p>
+          <h3 className={style.title}>ШАГ 1</h3>
+          <p className={style.subtitle}>Вы нажимаете свой профиль и выбираете пункт "Создать магазин"</p>
         </Fragment>
-        <Image src={"/images/laptop.webp"} alt='img' width={400} height={360}/>
+        <Image src={"/images/laptop.webp"} alt='img' width={600} height={360}/>
       </div>
     </section>
   );
