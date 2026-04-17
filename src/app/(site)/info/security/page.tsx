c0691eb93adc3a033373d042ec0e51e7b2f4214d
ArzubekDev <arzubekmain@gmail.com>
2026-04-17 16:04:28 +0600

    feat: securityPage

---
+++ src/app/(site)/info/security/page.tsx
@@ -0,0 +1,10 @@
+import Security from '@/src/components/pages/info/security'
+import React from 'react'
+
+const SecurityPage = () => {
+  return (
+    <div><Security/></div>
+  )
+}
+
+export default SecurityPage
