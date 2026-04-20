cdc2544a68a5c67692aa082d681cb2b542f5fd47
ArzubekDev <arzubekmain@gmail.com>
2026-04-20 10:46:14 +0600

    refactor: queryClient with try catch

--- src/app/(site)/(home)/page.tsx
+++ src/app/(site)/(home)/page.tsx
@@ -1,20 +1,22 @@
-import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
-import Home from "@/src/components/pages/home";
-import { getProducts } from "@/src/shared/api/product.api";
-import { PRODUCT_KEYS } from "@/src/shared/api/query-keys";
+import Home from '@/src/components/pages/home';
+import { getProducts } from '@/src/shared/api/product.api';
+import { PRODUCT_KEYS } from '@/src/shared/api/query-keys';
 
-export default async function HomePage () {
-    const queryClient = new QueryClient();
+export default async function HomePage() {
+  const queryClient = new QueryClient();
 
+  try {
     await queryClient.prefetchQuery({
-        queryKey: PRODUCT_KEYS.all,
-        queryFn: getProducts,
-    })
+      queryKey: PRODUCT_KEYS.all,
+      queryFn: getProducts,
+    });
+  } catch (error) {}
 
-    return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <Home/>
-        </HydrationBoundary>
-    )
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Home />
+    </HydrationBoundary>
+  );
 }
