186211e53bfaf0610b5b274a40a81b1faaeaf45e
ArzubekDev <arzubekmain@gmail.com>
2026-04-21 11:26:19 +0600

    feat: create entities

---
+++ src/entities/product/ui/SizeChartModal.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.module.scss';
+
+const SizeChartModal = () => {
+  return (
+    <section className={style.page}>
+      <h1>SizeChartModal</h1>
+    </section>
+  );
+};
+
+export default SizeChartModal;
