b0cb97c62312fc7ca98dce085051f95a451eb1f9
ArzubekDev <arzubekmain@gmail.com>
2026-04-21 11:51:37 +0600

    feat: create: sizechartmodal

--- src/entities/product/ui/SizeChartModal.tsx
+++ src/entities/product/ui/SizeChartModal.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+'use client'
+
+import React, { useState } from 'react';
+import { Button, Drawer } from 'antd';
+
 import style from './style.module.scss';
 
-const SizeChartModal = () => {
+const SizeChartModal: React.FC = () => {
+  const [open, setOpen] = useState(false);
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
   return (
     <section className={style.page}>
-      <h1>SizeChartModal</h1>
+      <Button type="primary" onClick={showDrawer}>
+        Open
+      </Button>
+      <Drawer
+        title="Drawer Closable Placement"
+        closable={{ placement: 'end' }}
+        onClose={onClose}
+        open={open}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Take a look at the top-right corner...</p>
+      </Drawer>
     </section>
   );
 };
