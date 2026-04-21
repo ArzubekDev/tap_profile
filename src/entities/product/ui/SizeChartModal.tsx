e3cc57f737e5132ce79fd5821fb7d810350da13f
ArzubekDev <arzubekmain@gmail.com>
2026-04-21 14:47:33 +0600

    refactor: SizeChartModal

--- src/entities/product/ui/SizeChartModal.tsx
+++ src/entities/product/ui/SizeChartModal.tsx
@@ -1,35 +1,57 @@
-'use client';
-
-import { Drawer } from 'antd';
-
-import { useWindowSize } from '@/src/shared/hooks/useWindowSize';
+import { Modal } from 'antd';
 import style from './style.module.scss';
 
 type SizeChartModalProps = {
-  open: boolean;
   close: () => void;
+  open: boolean;
   charts: any;
 };
-const SizeChartModal = ({ open, close, charts }: SizeChartModalProps) => {
-  const isCompact = useWindowSize(1000);
-  const placement = isCompact ? 'bottom' : 'right';
-  const drawerSize = isCompact ? { height: '80%' } : { width: 550 };
-
+const SizeChartModal = ({ close, open, charts }: SizeChartModalProps) => {
   return (
-    <section className={style.page}>
-      <Drawer
-        title="Таблица размеров"
-        placement={placement}
-        {...drawerSize}
-        closable={{ placement: 'end' }}
-        onClose={close}
-        open={open}
-      >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Take a look at the top-right corner...</p>
-      </Drawer>
-    </section>
+    <Modal
+      // footer={footer}
+      className={style.customModal}
+      title="Custom Function Modal"
+      mask={{ enabled: true, blur: true }}
+      open={open}
+      onOk={close}
+      onCancel={close}
+    >
+     <div className={style.sizeTable}>
+  {charts?.map((chart: any, index: any) => (
+    <div key={index} className={style.section}>
+      <h3 className={style.title}>{chart.title}</h3>
+      
+      {/* 1. БАШКЫ САПТЫ (HEADERS) КОШУУ */}
+      <div className={`${style.row} ${style.headerRow}`}>
+        {chart.headers.map((header: any, colIndex: any) => (
+          <div key={colIndex} className={`${style.cell} ${style.headerCell} ${colIndex === 2 ? style.active : ''}`}>
+            {typeof header === 'string' ? header : header.label}
+          </div>
+        ))}
+      </div>
+
+      {/* 2. МААЛЫМАТТАР (DATA) */}
+      {chart.data.map((row: any, rowIndex: any) => (
+        <div key={rowIndex} className={style.row}>
+          {chart.headers.map((header: any, colIndex: any) => {
+            const key = typeof header === 'string' ? header : header.key;
+            const isActive = colIndex === 2;
+            return (
+              <div
+                key={colIndex}
+                className={`${style.cell} ${isActive ? style.active : ''} ${colIndex === 0 ? style.firstCol : ''}`}
+              >
+                {row[key]}
+              </div>
+            );
+          })}
+        </div>
+      ))}
+    </div>
+  ))}
+</div>
+    </Modal>
   );
 };
 
