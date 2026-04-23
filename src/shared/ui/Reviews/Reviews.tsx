56716ea1e10b56b6d7f8a38feff53acd1cca6a1d
ArzubekDev <arzubekmain@gmail.com>
2026-04-23 12:02:54 +0600

    fix: tsconfig

---
+++ src/shared/ui/Reviews/Reviews.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.module.scss';
+
+const Reviews = () => {
+  return (
+    <section className={style.page}>
+      <div className={style.header}></div>
+    </section>
+  );
+};
+
+export default Reviews;
