39e3f193b6472ea22839251555732baeac3064f1
ArzubekDev <arzubekmain@gmail.com>
2026-04-23 16:32:50 +0600

    style: pagination

--- src/components/Icons/components/IconArrow.tsx
+++ src/components/Icons/components/IconArrow.tsx
@@ -21,7 +21,6 @@ const IconArrow: FC<TIcon> = ({ color, className, width, height, ...rest }) => {
       viewBox="0 0 14 14"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect width="14" height="14" fill="white" />
       <path d="M2 5L7 10L12 5" stroke="#4F4F4F" strokeWidth="1.5" strokeLinecap="round" />
     </svg>
   );
--- src/shared/ui/Pagination/Pagination.tsx
+++ src/shared/ui/Pagination/Pagination.tsx
@@ -1,30 +1,56 @@
+'use client';
+import { IconArrow } from '@/src/components/Icons';
+import { Select } from 'antd';
 import React from 'react';
 import style from './style.module.scss';
-import { IconArrow } from '@/src/components/Icons';
 
 const Pagination = () => {
+  const handleChange = (value: { value: string; label: React.ReactNode }) => {
+    console.log(value); // { value: "lucy", key: "lucy", label: "Lucy (101)" }
+  };
   return (
-    <div className={style.page}>
-      <div className={style.button}>
-        {[1, 2].map((el) => (
-          <IconArrow key={el} className={style.dubleArrowBack} />
-        ))}
+    <div className={style.pagination}>
+      <div className={style.page}>
+        <div className={style.button}>
+          {[1, 2].map((el) => (
+            <IconArrow key={el} className={style.dubleArrowBack} />
+          ))}
+        </div>
+
+        <div className={style.button}>
+          <IconArrow className={style.arrowBack} />
+        </div>
+
+        <div className={style.button}>1</div>
+
+        <div className={style.button}>
+          <IconArrow className={style.arrowNext} />
+        </div>
+
+        <div className={style.button}>
+          {[1, 2].map((el) => (
+            <IconArrow key={el} className={style.dubleArrowNext} />
+          ))}
+        </div>
       </div>
-
-      <div className={style.button}>
-        <IconArrow className={style.arrowBack} />
-      </div>
-
-      <div className={style.activeNumber}>1</div>
-
-      <div className={style.button}>
-        <IconArrow className={style.arrowNext} />
-      </div>
-
-      <div className={style.button}>
-        {[1, 2].map((el) => (
-          <IconArrow key={el} className={style.dubleArrowNext} />
-        ))}
+      <div className={style.select}>
+        <span>1-50 из 195 заказов</span>
+        <Select
+          labelInValue
+          defaultValue={{ value: '50', label: '50' }}
+          style={{ width: 120 }}
+          onChange={handleChange}
+          options={[
+            {
+              value: '50',
+              label: '50',
+            },
+            {
+              value: '51',
+              label: '51',
+            },
+          ]}
+        />
       </div>
     </div>
   );
