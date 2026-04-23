bb3ab070340ab3b29081e31eccd3791d44e1a42c
ArzubekDev <arzubekmain@gmail.com>
2026-04-23 15:54:50 +0600

    feat: Pagination

---
+++ src/shared/ui/Pagination/Pagination.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.module.scss';
+
+const Pagination = () => {
+  return (
+    <section className={style.page}>
+      <h1>Pagination</h1>
+    </section>
+  );
+};
+
+export default Pagination;
