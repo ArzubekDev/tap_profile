60958f2ae2de04374e5632502d6febedf798437a
ArzubekDev <arzubekmain@gmail.com>
2026-04-23 15:10:07 +0600

    fix: historyForm

--- src/components/pages/profile/history/historyForms/index.tsx
+++ src/components/pages/profile/history/historyForms/index.tsx
@@ -1,90 +1,84 @@
 'use client'
-import { Button, DatePickerProps } from "antd"
-import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { Button, Modal, Badge } from "antd";
+import { SearchOutlined, ControlOutlined } from "@ant-design/icons";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { DatePickerFormController, InputFormController, SelectFormController } from "@/src/components/form/Controllers"
+import { InputFormController, SelectFormController } from "@/src/components/form/Controllers";
 import { THistoryForm, ZhistoryForm } from "../zod/zod";
-
-import style from "./style.module.scss"
+import style from "./style.module.scss";
 
 const HistoryForms = () => {
-  const { control, handleSubmit, reset } = useForm<THistoryForm>({
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { control, handleSubmit, reset, watch } = useForm<THistoryForm>({
     resolver: zodResolver(ZhistoryForm),
     defaultValues: {
       name: '',
       status: 'all',
       store: 'all',
-      dateFrom: null,
-      dateTo: null,
     },
   });
 
-  const onSubmit = (data: THistoryForm) => console.log('history', data);
+  const onSubmit = (data: THistoryForm) => {
+    console.log(data);
+    setIsModalOpen(false);
+  };
+
   return (
-          <form onSubmit={handleSubmit(onSubmit)} className={style.form}>
-        <div className={style.top}>
-          {/* Имя */}
+    <div className={style.headerContainer}>
+      <form onSubmit={handleSubmit(onSubmit)} className={style.formWrapper}>
+        
+        <div className={style.searchBar}>
           <InputFormController
             id="searchName"
             name="name"
-            placeholder="Имя, фамилия, телефон..."
-            label="Имя"
+            placeholder="Поиск товаров"
+            prefix={<SearchOutlined style={{ color: '#8c8c8c' }} />}
             control={control}
-          />
-          {/* Статус */}
-          <SelectFormController
-            name="status"
-            control={control}
-            label="Статус"
-            options={statusOptions}
-            className={style.searchInput}
+            className={style.customInput}
           />
         </div>
-        {/* Магазины */}
-        <div className={style.center}>
-          <SelectFormController
-            name="store"
-            control={control}
-            label="Магазин"
-            options={storeOptions}
-            className={style.searchInput}
-          />
-        </div>
-        {/* Даты */}
-        <div className={style.bottom}>
-          <DatePickerFormController
-            name="dateFrom"
-            control={control}
-            label="Дата с"
-            placeholder="дд.мм.гггг"
-            format="DD.MM.YYYY"
-          />
-          <DatePickerFormController
-            name="dateTo"
-            control={control}
-            label="Дата по"
-            placeholder="дд.мм.гггг"
-            format="DD.MM.YYYY"
-          />
-        </div>
-        <div className={style.buttons}>
-          <Button type="primary" htmlType="submit" className={style.apply}>
-            Применить
-          </Button>
-          <Button onClick={() => reset()} className={style.reset}>
-            Сбросить
-          </Button>
-        </div>
+
+        <Button 
+          icon={<ControlOutlined />} 
+          onClick={() => setIsModalOpen(true)}
+          className={style.filterButton}
+        >
+          Все фильтры
+        </Button>
+
+        <Modal
+          title="Фильтры"
+          open={isModalOpen}
+          onOk={handleSubmit(onSubmit)}
+          onCancel={() => setIsModalOpen(false)}
+          okText="Применить"
+          cancelText="Сбросить"
+          okButtonProps={{ style: { backgroundColor: '#1890ff' } }}
+        >
+          <div className={style.modalFilters}>
+            <SelectFormController
+              name="status"
+              control={control}
+              label="Статус"
+              options={statusOptions}
+            />
+            <SelectFormController
+              name="store"
+              control={control}
+              label="Магазин"
+              options={storeOptions}
+            />
+          </div>
+        </Modal>
       </form>
-  )
-}
-
-export default HistoryForms
-
-const onChange: DatePickerProps['onChange'] = (date, dateString) => {
-  console.log(date, dateString);
+    </div>
+  );
 };
+
+export default HistoryForms;
 
 const statusOptions = [
   { value: 'all', label: 'Все статусы' },
