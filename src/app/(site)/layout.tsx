0df5f472d4bc2bcb381faf2b7ce01c1ccf81dfb6
ArzubekDev <arzubekmain@gmail.com>
2026-04-23 12:15:10 +0600

    layoutsite

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -3,17 +3,19 @@ import { ReactNode } from 'react';
 import Footer from '@/src/components/layout/footer';
 import Header from '@/src/components/layout/header';
 
+import style from "./style.module.scss"
+
 interface LayoutSiteProps {
   children: ReactNode;
 }
 
 const LayoutSite = ({ children }: LayoutSiteProps) => {
   return (
-    <>
+    <div className={style.layoutSite}>
       <Header />
       {children}
       <Footer/>
-    </>
+    </div>
   );
 };
 
