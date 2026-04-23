af00f4c580d48ada2dd71aa471b87e4358997aee
ArzubekDev <arzubekmain@gmail.com>
2026-04-23 17:08:47 +0600

    feat: slug

---
+++ src/app/(site)/basket/orders/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.module.scss';
+
+const Page = () => {
+  return (
+    <section className={style.page}>
+      <h1>Page</h1>
+    </section>
+  );
+};
+
+export default Page;
