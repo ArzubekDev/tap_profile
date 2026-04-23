5fba4b0c324f7108efba45c34bae13e7099dc207
ArzubekDev <arzubekmain@gmail.com>
2026-04-23 12:07:54 +0600

    feat: reviewPage

---
+++ src/app/(site)/reviews/page.tsx
@@ -0,0 +1,10 @@
+import Reviews from '@/src/shared/ui/Reviews/Reviews'
+import React from 'react'
+
+const ReviewsPage = () => {
+  return (
+    <div><Reviews/></div>
+  )
+}
+
+export default ReviewsPage
--- src/shared/ui/Reviews/Reviews.tsx
+++ src/shared/ui/Reviews/Reviews.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import style from './style.module.scss';
+import { IconStar } from '@/src/components/Icons';
 
 const Reviews = () => {
   return (
     <section className={style.page}>
-      <div className={style.header}></div>
+      <div className={style.header}>
+        <h3>Отзывы <span>23</span></h3>
+        <div><span>4.3</span> <span><IconStar/></span></div>
+      </div>
     </section>
   );
 };
