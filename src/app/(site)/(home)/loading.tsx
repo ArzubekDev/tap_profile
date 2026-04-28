f7c656a75bf38ac0415e99a006f79ee0e9b4a896
ArzubekDev <arzubekmain@gmail.com>
2026-04-28 10:34:36 +0600

    feat: (home)/loading.tsx

---
+++ src/app/(site)/(home)/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.module.scss';
+
+const Loading = () => {
+  return (
+    <section className={style.loading}>
+      <h1>Loading...</h1>
+    </section>
+  );
+};
+
+export default Loading;
