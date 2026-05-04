493a0b1fa839a103175ee9bb7c413caadafc3423
ArzubekDev <arzubekmain@gmail.com>
2026-05-04 13:53:54 +0600

    reaf: favorite-list

--- src/app/(site)/basket/favorite-list/loading.tsx
+++ src/app/(site)/basket/favorite-list/loading.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import style from './style.module.scss';
 
 const Loading = () => {
   return (
     <section className={style.loading}>
-      <h1>Loading...</h1>
+      <h1>Yldam Market</h1>
     </section>
   );
 };
