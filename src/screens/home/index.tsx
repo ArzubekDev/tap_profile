376cdb4c9cab02a2112b7c34e9cf1ea08e6859a2
ArzubekDev <arzubekmain@gmail.com>
2026-05-04 13:20:17 +0600

    size-chart-modal

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -8,7 +8,7 @@ import { PRODUCT_KEYS } from '@/src/shared/api/query-keys';
 
 import sizeCharts from '@/src/entities/product/size-charts/sizeCharts.json';
 import ProductCard from '@/src/entities/ProductCard/ProductCard';
-import SizeChartModal from '@/src/shared/ui/size-chart-modal/SizeChartModal';
+import SizeChartModal from '@/src/entities/size-chart-modal/SizeChartModal';
 
 import style from './style.module.scss';
 
